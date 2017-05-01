036f75e5b7205a968f64d8e5dc28d1b1c78b9488
chandan <chandanmadhesia@gmail.com>
2017-05-01 17:56:37 +0530

    added code for multipolygon

--- src/main/java/com/chandan/osmosis/plugin/geojson/converter/OsmRelationToMultipolygonConverter.java
+++ src/main/java/com/chandan/osmosis/plugin/geojson/converter/OsmRelationToMultipolygonConverter.java
@@ -10,6 +10,7 @@ import org.openstreetmap.osmosis.core.domain.v0_6.EntityType;
 import org.openstreetmap.osmosis.core.domain.v0_6.Relation;
 import org.openstreetmap.osmosis.core.domain.v0_6.RelationMember;
 
+import javax.sound.sampled.Line;
 import java.util.*;
 
 /**
@@ -35,8 +36,8 @@ public class OsmRelationToMultipolygonConverter implements OsmToFeatureConverter
 	@Override
 	public List<Feature<Polygon>> convert(Relation relation) {
 		List<Feature<Polygon>> polygons = new ArrayList<>();
-		Map<Long, LineStringUsage> outerStartNodeIdLineStringMap = new HashMap<>();
-		Map<Long, LineStringUsage> innerStartNodeIdLineStringMap = new HashMap<>();
+		Map<Long, Feature<LineString>> outerStartNodeIdLineStringMap = new HashMap<>();
+		Map<Long, Feature<LineString>> innerStartNodeIdLineStringMap = new HashMap<>();
 		Iterator<RelationMember> relationMembersIt = relation.getMembers().iterator();
 
 		while (relationMembersIt.hasNext()){
@@ -54,24 +55,23 @@ public class OsmRelationToMultipolygonConverter implements OsmToFeatureConverter
 		return polygons;
 	}
 
-	private void addPolygonsFromLineString(Map<Long, LineStringUsage> lineStringUsageMap, List<Feature<Polygon>> polygons) {
+	private void addPolygonsFromLineString(Map<Long, Feature<LineString>> lineStringUsageMap, List<Feature<Polygon>> polygons) {
 		Set<Long> startNodeIds = lineStringUsageMap.keySet();
 		while (!startNodeIds.isEmpty()) {
 			List<Coordinate> ring = new ArrayList<>();
 			final Long ringGroupStartNodeId = startNodeIds.iterator().next();
-			LineStringUsage lineStringUsage = lineStringUsageMap.get(ringGroupStartNodeId);
-			if (lineStringUsage == null) {
+			Feature<LineString> lineString = lineStringUsageMap.get(ringGroupStartNodeId);
+			if (lineString == null) {
 				throw new IllegalArgumentException("Polygon incomplete");
 			}
-			Long currentEndNodeId = Utils.getEndNode(lineStringUsage.lineStringFeature);
+			Long currentEndNodeId = Utils.getEndNode(lineString);
 			while (!currentEndNodeId.equals(ringGroupStartNodeId)) {
-				lineStringUsage.usageCount++;
-				ring.addAll(lineStringUsage.lineStringFeature.getGeometry().getCoordinates());
-				lineStringUsage = lineStringUsageMap.get(currentEndNodeId);
-				if (lineStringUsage == null) {
+				ring.addAll(lineString.getGeometry().getCoordinates());
+				lineString = lineStringUsageMap.get(currentEndNodeId);
+				if (lineString == null) {
 					throw new IllegalArgumentException("Polygon incomplete");
 				}
-				currentEndNodeId = Utils.getEndNode(lineStringUsage.lineStringFeature);
+				currentEndNodeId = Utils.getEndNode(lineString);
 			}
 			if (!currentEndNodeId.equals(ringGroupStartNodeId)) {
 				throw new IllegalArgumentException("Polygon incomplete");
@@ -84,15 +84,18 @@ public class OsmRelationToMultipolygonConverter implements OsmToFeatureConverter
 							.build());
 			startNodeIds.remove(ringGroupStartNodeId);
 		}
+		if (!startNodeIds.isEmpty()) {
+			throw new IllegalArgumentException("Polygon incomplete");
+		}
 	}
 
 	private void handleRelationMember(RelationMember relationMember,
-			Map<Long, LineStringUsage> lineStringUsageMap, List<Feature<Polygon>> preexistingPolygons) {
+			Map<Long, Feature<LineString>> lineStringMap, List<Feature<Polygon>> preexistingPolygons) {
 		if (relationMember.getMemberType() == EntityType.Way) {
 			Feature<LineString> lineStringFeature = featureLinestringCache.get(relationMember.getMemberId());
 			if (lineStringFeature != null) {
-				lineStringUsageMap.put(Utils.getStartNode(lineStringFeature),
-						new LineStringUsage(lineStringFeature));
+				lineStringMap.put(Utils.getStartNode(lineStringFeature),
+						lineStringFeature);
 				return;
 			}
 			Feature<Polygon> polygonFeature = featurePolygonCache.get(relationMember.getMemberId());
@@ -110,5 +113,4 @@ public class OsmRelationToMultipolygonConverter implements OsmToFeatureConverter
 			this.lineStringFeature = lineStringFeature;
 		}
 	}
-
 }
